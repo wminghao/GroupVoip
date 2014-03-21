449b8775633f288b0c13002bc3ac08930713975d
wminghao <wminghao@users.noreply.github.com>
2014-03-21 11:11:18 -0700

    more code for mixing

--- server/mixcoder/prog/VideoMixer.cpp
+++ server/mixcoder/prog/VideoMixer.cpp
@@ -22,10 +22,76 @@ SmartPtr<SmartBuffer> VideoMixer::mixStreams(SmartPtr<SmartBuffer> planes[][3],
                                              VideoStreamSetting* settings, 
                                              int totalStreams)
 {
+    SmartPtr<SmartBuffer> result;
     if( tryToInitSws(settings, totalStreams) ) {
-        //TODO do mixing
+        int scaledWidth = mappingToScalingWidth(totalStreams);
+        int scaledHeight = mappingToScalingHeight(totalStreams);
+
+        SmartPtr<SmartBuffer> scaledVideoPlanes[MAX_XCODING_INSTANCES][3];
+        int scaledVideoStrides[MAX_XCODING_INSTANCES][3];
+
+        for(int i=0; i<MAX_XCODING_INSTANCES; i++) {
+            scaledVideoPlanes[i][0] = new SmartBuffer( scaledWidth* scaledHeight );
+            scaledVideoPlanes[i][1] = new SmartBuffer( ( scaledWidth * scaledHeight  + 4) / 4 );
+            scaledVideoPlanes[i][2] = new SmartBuffer( ( scaledWidth * scaledHeight  + 4) / 4 );
+            scaledVideoStrides[i][0] = scaledWidth;
+            scaledVideoStrides[i][1] = scaledWidth/2;
+            scaledVideoStrides[i][2] = scaledWidth/2;
+            
+            u8* inputPlanes[3];
+            inputPlanes[0] = planes[i][0]->data();
+            inputPlanes[1] = planes[i][1]->data();
+            inputPlanes[2] = planes[i][2]->data();
+
+            u8* scaledPlanes[3];
+            scaledPlanes[0] = scaledVideoPlanes[i][0]->data();
+            scaledPlanes[1] = scaledVideoPlanes[i][1]->data();
+            scaledPlanes[2] = scaledVideoPlanes[i][2]->data();
+            sws_scale( swsCtx_[i], inputPlanes, strides[i], 0, settings[i].height,
+                       scaledPlanes, scaledVideoStrides[i]);
+        }
+
+        if(totalStreams == 1) {
+            int outputWidth = outputSetting_.width;
+            int outputHeight = outputSetting_.height;
+
+            result = new SmartBuffer( outputWidth*outputHeight*3/2 );
+
+            //convert from AV_PIX_FMT_YUV420P                                                                                                                                                                                                                         
+            //3 planes combined into 1 buffer                                                                                                                                                                                                                         
+            u8* in = scaledVideoPlanes[0][0]->data();
+            u8* out = result->data();
+            u32 offsetOut = 0;
+                                                                                                                                                                                                                                                                                         u32 bytesPerLineInY = scaledVideoStrides[0][0];                                                                                                                                                                                                                
+            u32 offsetInY = 0;                                                                                                                                                                                                                                        
+            for(int i = 0; i < outputHeight; i ++ ) {                                                                                                                                                                                                                    
+                memcpy( out+offsetOut, in+offsetInY, outputWidth);                                                                                                                                                                                                       
+                offsetInY += bytesPerLineInY;                                                                                                                                                                                                                         
+                offsetOut += outputWidth;                                                                                                                                                                                                                                
+            }                                                                                                                                                                                                                                                         
+
+            in = scaledVideoPlanes[0][1]->data();
+            u32 bytesPerLineInU = scaledVideoStrides[0][1];                                                                                                                                                                                                                
+            u32 offsetInU = 0;                                                                                                                                                                                                                                        
+            for(int i = 0; i < outputHeight/2; i ++ ) {                                                                                                                                                                                                                  
+                memcpy( out+offsetOut, in+offsetInU, outputWidth/2);                                                                                                                                                                                                     
+                offsetInU += bytesPerLineInU;                                                                                                                                                                                                                         
+                offsetOut += outputWidth/2;                                                                                                                                                                                                                              
+            }                                                                                                                                                                                                                                                         
+
+            in = scaledVideoPlanes[0][2]->data();
+            u32 bytesPerLineInV = scaledVideoStrides[0][2];
+            u32 offsetInV = 0;                                                                                                                                                                                                                                        
+            for(int i = 0; i < outputHeight/2; i ++ ) {                                                                                                                                                                                                                  
+                memcpy( out+offsetOut, in+offsetInV, outputWidth/2);                                                                                                                                                                                                     
+                offsetInV += bytesPerLineInV;                                                                                                                                                                                                                         
+                offsetOut += outputWidth/2;                                                                                                                                                                                                                              
+            }
+        } else {
+            //TODO do mixing for other cases
+        }
     }
-    return new SmartBuffer(4, "TODO");
+    return result;
 }
 VideoMixer::~VideoMixer()
 {
