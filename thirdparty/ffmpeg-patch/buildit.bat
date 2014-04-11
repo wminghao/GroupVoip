a4132a229f334607ef7af7dfc9d76c57ba9817e6
wminghao <wminghao@users.noreply.github.com>
2014-04-11 16:51:00 -0700

    added patched version of ffmpeg for libavformat library

---
+++ thirdparty/ffmpeg-patch/buildit.bat
@@ -0,0 +1,3 @@
+rm ~/Develop/vlc-iOS/VLCKit/MobileVLCKit/ImportedSources/vlc/contrib/tarballs/ffmpeg-HEAD.tar.gz
+tar cvzf libav-HEAD-254c95c.tar.gz libav-HEAD-254c95c
+cp libav-HEAD-254c95c.tar.gz ~/Sites/ffmpeg-patch/
