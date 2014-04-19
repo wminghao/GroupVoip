a2d29e6073e608490ffd41a9df26838ede8b822f
wminghao <wminghao@users.noreply.github.com>
2014-04-19 15:40:06 -0700

    add all the libraries

---
+++ ios-client/cplibfiles.bat
@@ -0,0 +1,5 @@
+mkdir vlclibs
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos vlclibs/
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphonesimulator vlclibs/
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphoneos vlclibs/
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphonesimulator vlclibs/
