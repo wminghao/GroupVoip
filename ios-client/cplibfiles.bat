762fb940058a1bbaf4d06606332a4beb56561cc4
wminghao <wminghao@users.noreply.github.com>
2014-04-21 08:05:50 -0700

    cleanup the path

--- ios-client/cplibfiles.bat
+++ ios-client/cplibfiles.bat
@@ -1,5 +1,12 @@
 mkdir vlclibs
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos vlclibs/
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphonesimulator vlclibs/
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphoneos vlclibs/
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphonesimulator vlclibs/
+cd vlclibs
+mkdir Release-iphoneos
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/libMobileVLCKit.a Release-iphoneos
+mkdir Release-iphoneos
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphonesimulator/libMobileVLCKit.a Release-iphonesimulator
+mkdir Release-iphoneos
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphoneos/libMobileVLCKit.a Debug-iphoneos
+mkdir Release-iphoneos
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphonesimulator/libMobileVLCKit.a Debug-iphonesimulator
+cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/include .
+cd ..
