7f427db7223a1be7cf2c444bbaa01df4d5fbb9ab
wminghao <wminghao@users.noreply.github.com>
2014-04-21 08:16:22 -0700

    modified buildscript again

--- ios-client/cplibfiles.bat
+++ ios-client/cplibfiles.bat
@@ -2,11 +2,11 @@ mkdir vlclibs
 cd vlclibs
 mkdir Release-iphoneos
 cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/libMobileVLCKit.a Release-iphoneos
-mkdir Release-iphoneos
+mkdir Release-iphonesimulator
 cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphonesimulator/libMobileVLCKit.a Release-iphonesimulator
-mkdir Release-iphoneos
+mkdir Debug-iphoneos
 cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphoneos/libMobileVLCKit.a Debug-iphoneos
-mkdir Release-iphoneos
+mkdir Debug-iphonesimulator
 cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphonesimulator/libMobileVLCKit.a Debug-iphonesimulator
 cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/include .
 cd ..
