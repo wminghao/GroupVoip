902aaebc5e4efd79d62930d046734ee8b8e59ac0
wminghao <wminghao@gmail.com>
2014-04-23 21:36:54 -0700

    tried to cut down binary size

---
+++ thirdparty/vlc-shrink/cpit.bat
@@ -0,0 +1,2 @@
+cp ffmpeg-shrink/rules.mak /Users/wminghao/Develop/vlc-iOS/VLCKit/MobileVLCKit/ImportedSources/vlc/contrib/src/ffmpeg/rules.mak
+cp plugin-shrink/vlc-plugins.xcconfig /Users/wminghao/Develop/vlc-iOS/VLCKit/MobileVLCKit/vlc-plugins.xcconfig
