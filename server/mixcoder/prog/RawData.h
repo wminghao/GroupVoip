46428cde7197929eb2335adbd85c572ffb67f6d9
Ubuntu <ubuntu@ip-172-31-8-117.us-west-2.compute.internal>
2014-07-16 23:24:19 +0000

    forgot a file

---
+++ server/mixcoder/prog/RawData.h
@@ -0,0 +1,25 @@
+#ifndef __RAWDATA_H__
+#define __RAWDATA_H__
+
+#include "CodecInfo.h"
+#include "fwk/SmartBuffer.h"
+
+class VideoRawData;
+class VideoRawData: public SmartPtrInterface<VideoRawData> {
+ public:
+    SmartPtr<SmartBuffer> rawVideoPlanes_[3];
+    int rawVideoStrides_[3];
+    VideoStreamSetting rawVideoSettings_;
+    u32 pts; //pts == dts
+    int sp; //special properties, for avc, it's either sps/pps or nalu, for aac, it's sequenceheader or nalu, for other codecs, it's raw data
+};
+
+class AudioRawData;
+class AudioRawData: public SmartPtrInterface<AudioRawData>{
+ public:
+    SmartPtr<SmartBuffer> rawAudioFrame_;
+    bool bIsValid;
+    StreamSource ss; //special flag, mobile or desktop stream
+    u32 pts; //pts == dts
+};
+#endif //__RAWDATA_H__
